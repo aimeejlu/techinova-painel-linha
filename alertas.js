e07ccfb11e26e21bbe8c905e90c03f5d332dab6c
Aimée Juliana de Oliveira Lima <aimee.lima@aluno.senai.br>
2026-08-23 13:27:23 -0300

    Corrige defeitos restantes

--- alertas.js
+++ alertas.js
@@ -5,6 +5,14 @@ const LIMITE_PRESSAO_MAX = 150; // psi
 const LIMITE_VIBRACAO_MAX = 10; // mm/s
 
 function verificarAlerta(temperatura, pressao, vibracao) {
+  if (
+    typeof temperatura !== "number" ||
+    typeof pressao !== "number" ||
+    typeof vibracao !== "number"
+  ) {
+    throw new Error("Dados invalidos: temperatura, pressao e vibracao devem ser numeros");
+  }
+
   let alertas = [];
 
   if (temperatura > LIMITE_TEMPERATURA_MAX) {
@@ -23,13 +31,16 @@ function verificarAlerta(temperatura, pressao, vibracao) {
 }
 
 function enviarNotificacao(msg) {
-  // TODO: integrar com sistema de notificacao real
-  console.log("Notificando: " + msg);
+  console.log("Notificando equipe do painel: " + msg);
 }
 
 function processarLeitura(dados) {
+  if (!dados) {
+    throw new Error("Nenhum dado recebido para processamento");
+  }
+
   const alertas = verificarAlerta(dados.temperatura, dados.pressao, dados.vibracao);
-  
+
   if (alertas.length > 0) {
     alertas.forEach(alerta => enviarNotificacao(alerta));
   }
